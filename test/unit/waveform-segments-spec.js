4f8be8f8ab268dbab828c67bda4d33ed6da75b45
Thomas Parisot <thomas.parisot@bbc.co.uk>
2014-01-24 18:42:46 +0000

    Added segment testsuite

---
+++ test/unit/waveform-segments-spec.js
@@ -0,0 +1,20 @@
+define(['main'], function(peaks){
+
+  describe("player/waveform/waveform.segments", function () {
+
+    describe("addSegment", function(){
+      it("should accept spreaded arguments (soon deprecated)", function(){
+
+      });
+
+      it("should accept an array of Segment objects", function(){
+
+      });
+
+      it("should paint once, and not after each segment addition", function(){
+
+      });
+    });
+  });
+
+});
