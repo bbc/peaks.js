d944c1afb813225799f76385ac3ca2ea2df131b3
Thomas Parisot <thomas.parisot@bbc.co.uk>
2014-02-14 15:47:24 +0000

    Covering zoom levels.

--- test/unit/api-spec.js
+++ test/unit/api-spec.js
@@ -40,25 +40,6 @@ define(['main', 'EventEmitter', 'Kinetic'], function(Peaks, EventEmitter, Kineti
       expect(p).to.be.an.instanceOf(EventEmitter);
     });
 
-    //@see https://github.com/bbcrd/peaks.js/issues/9
-    xit("should return the correct time for time.getCurrentTime()", function (done) {
-      //Peaks.player.player.currentTime = 6;
-      document.querySelector('audio').currentTime = 6;
-
-      setTimeout(function(){
-        expect(p.time.getCurrentTime()).to.equal(6);
-        done();
-      }, 500);
-    });
-
-    it("should be able to set and return the zoom level", function () {
-      expect(p.zoom.getZoom()).to.equal(0);
-
-      p.zoom.setZoom(1);
-
-      expect(p.zoom.getZoom()).to.equal(1);
-    });
-
     it("should be able to add and return segments", function (done) {
 
       var testSeg = {
@@ -78,100 +59,6 @@ define(['main', 'EventEmitter', 'Kinetic'], function(Peaks, EventEmitter, Kineti
 
         done();
       }, 300);
-    });
-
-    xit("should display the correct waveform for the test data", function () {
-
-      var zoomCanvas, sourceImageData, testImgOne, testImgTwo;
-
-      /**
-       * We've copied this from imagediff.js in
-       * https://github.com/HumbleSoftware/js-imagediff and modified
-       * it to return a percentage that can then be used to test
-       * against. This is due to the fact that there seem to be
-       * rounding errors between the results of .toDataURL and
-       * .toImageData() for HTML5 canvas element, this prevented us
-       * from using the standard imagediff framework.
-       */
-      var imageDataEqual = function (a, b, tolerance) {
-        var helpers = {
-          equalWidth: function (a, b) {
-            return a.width === b.width;
-          },
-          equalHeight: function (a, b) {
-            return a.height === b.height;
-          },
-          equalDimensions: function (a, b) {
-            return helpers.equalHeight(a, b) && helpers.equalWidth(a, b);
-          }
-        };
-        var aData     = a.data,
-            bData     = b.data,
-            length    = aData.length,
-            different = 0,
-            i;
-        tolerance = tolerance || 0;
-        if (!helpers.equalDimensions(a, b)) return false;
-        for (i = length; i--;) {
-          if (aData[i] !== bData[i]) {
-            different++;
-          }
-        }
-        if (different > 0 && (different/length) > tolerance) return false;
-        return true;
-      };
-
-      var getImageDataFromCanvas = function (canvas) {
-        return canvas.getContext("2d").getImageData(0,0,canvas.width,canvas.height);
-      };
-
-      var getExpectedImageData = function (image) {
-        var expectedCanvas = document.createElement("canvas");
-        expectedCanvas.width = zoomCanvas.width;
-        expectedCanvas.height = zoomCanvas.height;
-        expectedCanvas.getContext("2d").drawImage(image, 0, 0);
-        return getImageDataFromCanvas(expectedCanvas);
-      };
-
-      var getSourceImageData = function () {
-        return getImageDataFromCanvas(zoomCanvas);
-      };
-
-      // Load up images and source data
-      runs(function () {
-        zoomCanvas = $("#zoom-container canvas").first()[0];
-
-        window.peaks.waveform.waveformZoomView.updateZoomWaveform(0); // Ensure waveform offset is set to 0
-        sourceImageData = getSourceImageData();
-
-        testImgZeroOffset = new Image();
-        testImgZeroOffset.src = "base/test/test_img/zoom_0_offset.png";
-        testImgTenThouOffset = new Image();
-        testImgTenThouOffset.src = "base/test/test_img/zoom_10000_offset.png";
-      });
-
-      // Wait for images to load
-      waitsFor(function () {
-        return testImgZeroOffset.complete && testImgTenThouOffset.complete;
-      }, "Image(s) not loaded", 2000);
-
-      // Compare expected images against generated waveform
-      runs(function () {
-        // Test waveform at 0 offset
-        expect(
-          imageDataEqual(getExpectedImageData(testImgZeroOffset), sourceImageData, 0.01)
-        ).toBe(true);
-
-        // Set to 10000 offset
-        window.peaks.waveform.waveformZoomView.updateZoomWaveform(10000);
-        sourceImageData = getSourceImageData(); // Reload imageData at new offset
-
-        // Test waveform at 10000 offset
-        expect(
-          imageDataEqual(getExpectedImageData(testImgTenThouOffset), sourceImageData, 0.01)
-        ).toBe(true);
-      });
-
     });
 
   });
---
+++ test/unit/api-zoom-spec.js
@@ -0,0 +1,110 @@
+define(['main', 'EventEmitter', 'underscore', 'Kinetic'], function(Peaks, EventEmitter, _, Kinetic){
+  describe("Peaks.zoom", function () {
+
+    var p, sandbox;
+
+    /**
+     * SETUP =========================================================
+     */
+
+    beforeEach(function (done) {
+      loadAllFixtures();
+      sandbox = sinon.sandbox.create();
+
+      p = Peaks.init({
+        container: document.getElementById('waveform-visualiser-container'),
+        audioElement: document.querySelector('audio'),
+        dataUri: 'base/test_data/sample.dat',
+        keyboard: true,
+        height: 240
+      });
+
+      setTimeout(done, 500);
+    });
+
+    /**
+     * TEARDOWN ======================================================
+     */
+
+    afterEach(function (done) {
+      sandbox.restore();
+      removeAllFixtures();
+      setTimeout(done, 200);
+    });
+
+    /**
+     * TESTS =========================================================
+     */
+
+    describe("getZoom", function(){
+      it("should be able to get the initial zoom level value, which is 0", function(){
+        expect(p.zoom.getZoom()).to.equal(0);
+      });
+    });
+
+    describe("setZoom", function(){
+      it("should update the zoom level value", function (){
+        p.zoom.setZoom(1);
+
+        expect(p.zoom.getZoom()).to.equal(1);
+      });
+
+      it("should dispatch waveform_zoom_level_changed with the value associated to the zoom level", function (){
+        var spy = sandbox.spy();
+
+        p.on("waveform_zoom_level_changed", spy);
+        p.zoom.setZoom(1);
+
+        expect(spy.calledWith(1024)).to.equal(true);
+      });
+
+      it("should be inefficient if the zoom level index is a negative value", function (){
+        p.zoom.setZoom(-1);
+
+        expect(p.zoom.getZoom()).to.equal(0);
+      });
+
+      it("should be inefficient if the zoom level index is an inexisting index", function (){
+        p.options.zoomLevels = [512, 1024];
+
+        p.zoom.setZoom(2);
+
+        expect(p.zoom.getZoom()).to.equal(1);
+      });
+
+      it("should throw a RangeError if an existing zoom level does not have sufficient data", function (){
+        expect(function(){
+          p.zoom.setZoom(2);
+        }).to.throw(RangeError);
+      });
+    });
+
+    describe("zoomOut", function(){
+      it("should call setZoom with a bigger zoom level", function(){
+        var spy = sandbox.spy();
+        var zoomLevel = p.currentZoomLevel;
+
+        p.on("waveform_zoom_level_changed", spy);
+        p.zoom.zoomOut();
+
+        expect(spy.getCall(0).args).to.deep.equal([ p.options.zoomLevels[zoomLevel + 1] ]);
+      });
+    });
+
+    describe("zoomIn", function(){
+      it("should call setZoom with a smaller zoom level", function(){
+        p.zoom.setZoom(1);
+
+        var spy = sandbox.spy();
+        var zoomLevel = p.currentZoomLevel;
+
+        p.on("waveform_zoom_level_changed", spy);
+        p.zoom.zoomIn();
+
+        expect(spy.getCall(0).args).to.deep.equal([ p.options.zoomLevels[zoomLevel - 1] ]);
+      });
+    });
+
+  });
+
+});
