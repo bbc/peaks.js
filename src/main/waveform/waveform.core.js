5b874103c1484f91a58ff7a59da9414046f3e0ee
Kyle <kylewhite@gmail.com>
2014-10-01 13:21:43 -0400

    Removed check for undefined waveformBuilderOptions

--- src/main/waveform/waveform.core.js
+++ src/main/waveform/waveform.core.js
@@ -84,11 +84,7 @@ define([
         xhr.onload = function(response) {
           if (this.readyState === 4 && this.status === 200) {
             if (builder){
-              if(options.waveformBuilderOptions) {
                 WaveformData.builders[builder](response.target.response, options.waveformBuilderOptions, that.handleRemoteData.bind(that));
-              } else {
-                WaveformData.builders[builder](response.target.response, that.handleRemoteData.bind(that));
-              }
             }
             else {
               that.handleRemoteData(response.target, xhr);
