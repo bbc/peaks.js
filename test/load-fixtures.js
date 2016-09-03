c8a9080413cc21829152576601a868c849dfb35c
Chris Needham <chris.needham@bbc.co.uk>
2016-09-03 10:39:21 +0100

    Fixed eslint errors

--- test/load-fixtures.js
+++ test/load-fixtures.js
@@ -1,11 +1,14 @@
-function loadFixtures(filename){
+function loadFixtures(filename) {
   var el = document.createElement('div');
+
   el.id = 'fixtures-container-' + filename;
-  el.innerHTML = window.__html__['test/'+ filename +'.html'].replace('localhost', window.location.hostname);
+  el.innerHTML = window.__html__['test/' + filename + '.html']
+                       .replace('localhost', window.location.hostname);
   document.body.appendChild(el);
 
   return el;
 }
 
 var fixtureFiles = ['audioElement', 'waveformContainer'];
+
 fixtureFiles.forEach(loadFixtures);
