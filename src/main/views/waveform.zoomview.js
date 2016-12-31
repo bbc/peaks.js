a13464f4d1234b9e24f165c3db311de9702a3ba1
Jay Tolla <jay@getpillar.com>
2016-12-30 21:31:50 -0500

    Remove trailing spaces

--- src/main/views/waveform.zoomview.js
+++ src/main/views/waveform.zoomview.js
@@ -104,7 +104,7 @@ define([
 
         if (newFrameOffset !== this.initialFrameOffset) {
           self.peaks.emit('user_scroll.zoomview', newFrameOffset);
-           
+
           if (self.peaks.options.playedOverlay) {
             self.highlightPlayedRect.setAttr('width', self.playheadPixel - newFrameOffset);
           }
