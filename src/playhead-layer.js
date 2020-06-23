d7329656369520c2a7123fbf1d9a54f981327a02
Seoung Ho Jeong <shj5508@naver.com>
2020-06-23 23:05:03 +0900

    put playheadText left if playhead is in right end (#322)
    Put playhead text left if playhead is near the right edge
    
    Prevent hiding the playhead text by moving it to the left of the playhead marker

--- src/playhead-layer.js
+++ src/playhead-layer.js
@@ -187,8 +187,16 @@ define([
 
       if (this._playheadText) {
         var text = this._view.formatTime(time);
+        var playheadTextWidth = this._playheadText.getTextWidth();
 
         this._playheadText.setText(text);
+
+        if (playheadTextWidth + playheadX > width - 2) {
+          this._playheadText.setAttr('x', -playheadTextWidth - 2);
+        }
+        else if (playheadTextWidth + playheadX < width) {
+          this._playheadText.setAttr('x', 2);
+        }
       }
 
       this._playheadLayer.draw();
