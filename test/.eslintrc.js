e9fac6fe8c724f12e1617ca2a0135346a2d6e3dc
Chris Needham <chris.needham@bbc.co.uk>
2020-07-13 09:49:46 +0100

    Fixed eslint rules

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "mocha": true,
-    "node": true,
+  'env': {
+    'browser': true,
+    'mocha': true,
+    'node': true
   },
-  "extends": "../.eslintrc.js",
-  "globals": {
-    "expect": false,
-    "sinon": false,
-    "peaks": false
+  'extends': '../.eslintrc.js',
+  'globals': {
+    'expect': false,
+    'sinon': false,
+    'peaks': false
   },
-  "rules": {
-    "brace-style": ["error", "stroustrup", { "allowSingleLine": true }],
+  'rules': {
+    'brace-style': ['error', 'stroustrup', { 'allowSingleLine': true }],
     // Disable no-unused-expressions for chai expectations
-    "no-unused-expressions": "off",
-    "newline-after-var": "off",
-    "max-len": "off"
+    'no-unused-expressions': 'off',
+    'newline-after-var': 'off',
+    'max-len': 'off'
   }
 };
