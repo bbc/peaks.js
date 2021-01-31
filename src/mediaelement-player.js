9971f5482008607a99c16f7470b187cf8b3ce240
Chris Needham <chris.needham@bbc.co.uk>
2021-01-31 10:53:43 +0000

    Refactored Player and MediaElementPlayer
    Move functions outside of constructor

--- src/mediaelement-player.js
+++ src/mediaelement-player.js
@@ -27,103 +27,105 @@ define([
     self._peaks = peaks;
     self._listeners = [];
     self._mediaElement = mediaElement;
-
-    /**
-     * Adds an event listener to the media element.
-     *
-     * @private
-     * @param {String} type The event type to listen for.
-     * @param {Function} callback An event handler function.
-     */
-
-    MediaElementPlayer.prototype._addMediaListener = function(type, callback) {
-      this._listeners.push({ type: type, callback: callback });
-      this._mediaElement.addEventListener(type, callback);
-    };
-
-    MediaElementPlayer.prototype.init = function(player) {
-      self._player = player;
-      self._listeners = [];
-      self._duration = self.getDuration();
-      self._isPlaying = false;
-
-      self._addMediaListener('timeupdate', function() {
-        self._peaks.emit('player.timeupdate', self.getCurrentTime());
-      });
-
-      self._addMediaListener('play', function() {
-        self._isPlaying = true;
-        self._peaks.emit('player.play', self.getCurrentTime());
-      });
-
-      self._addMediaListener('pause', function() {
-        self._isPlaying = false;
-        self._peaks.emit('player.pause', self.getCurrentTime());
-      });
-
-      self._addMediaListener('seeked', function() {
-        self._peaks.emit('player.seeked', self.getCurrentTime());
-      });
-
-      self._addMediaListener('canplay', function() {
-        self._peaks.emit('player.canplay');
-      });
-
-      self._addMediaListener('error', function(event) {
-        self._peaks.emit('player.error', event.target.error);
-      });
-
-      self._interval = null;
-    };
-
-    /**
-     * Cleans up the player object, removing all event listeners from the
-     * associated media element.
-     */
-
-    MediaElementPlayer.prototype.destroy = function() {
-      for (var i = 0; i < this._listeners.length; i++) {
-        var listener = this._listeners[i];
-
-        this._mediaElement.removeEventListener(
-          listener.type,
-          listener.callback
-        );
-      }
-
-      this._listeners.length = 0;
-
-      this._mediaElement = null;
-    };
-
-    MediaElementPlayer.prototype.play = function() {
-      return this._mediaElement.play();
-    };
-
-    MediaElementPlayer.prototype.pause = function() {
-      this._mediaElement.pause();
-    };
-
-    MediaElementPlayer.prototype.isPlaying = function() {
-      return this._isPlaying;
-    };
-
-    MediaElementPlayer.prototype.isSeeking = function() {
-      return this._mediaElement.seeking;
-    };
-
-    MediaElementPlayer.prototype.getCurrentTime = function() {
-      return this._mediaElement.currentTime;
-    };
-
-    MediaElementPlayer.prototype.getDuration = function() {
-      return this._mediaElement.duration;
-    };
-
-    MediaElementPlayer.prototype.seek = function(time) {
-      this._mediaElement.currentTime = time;
-    };
   }
+
+  /**
+   * Adds an event listener to the media element.
+   *
+   * @private
+   * @param {String} type The event type to listen for.
+   * @param {Function} callback An event handler function.
+   */
+
+  MediaElementPlayer.prototype._addMediaListener = function(type, callback) {
+    this._listeners.push({ type: type, callback: callback });
+    this._mediaElement.addEventListener(type, callback);
+  };
+
+  MediaElementPlayer.prototype.init = function(player) {
+    var self = this;
+
+    self._player = player;
+    self._listeners = [];
+    self._duration = self.getDuration();
+    self._isPlaying = false;
+
+    self._addMediaListener('timeupdate', function() {
+      self._peaks.emit('player.timeupdate', self.getCurrentTime());
+    });
+
+    self._addMediaListener('play', function() {
+      self._isPlaying = true;
+      self._peaks.emit('player.play', self.getCurrentTime());
+    });
+
+    self._addMediaListener('pause', function() {
+      self._isPlaying = false;
+      self._peaks.emit('player.pause', self.getCurrentTime());
+    });
+
+    self._addMediaListener('seeked', function() {
+      self._peaks.emit('player.seeked', self.getCurrentTime());
+    });
+
+    self._addMediaListener('canplay', function() {
+      self._peaks.emit('player.canplay');
+    });
+
+    self._addMediaListener('error', function(event) {
+      self._peaks.emit('player.error', event.target.error);
+    });
+
+    self._interval = null;
+  };
+
+  /**
+   * Cleans up the player object, removing all event listeners from the
+   * associated media element.
+   */
+
+  MediaElementPlayer.prototype.destroy = function() {
+    for (var i = 0; i < this._listeners.length; i++) {
+      var listener = this._listeners[i];
+
+      this._mediaElement.removeEventListener(
+        listener.type,
+        listener.callback
+      );
+    }
+
+    this._listeners.length = 0;
+
+    this._mediaElement = null;
+  };
+
+  MediaElementPlayer.prototype.play = function() {
+    return this._mediaElement.play();
+  };
+
+  MediaElementPlayer.prototype.pause = function() {
+    this._mediaElement.pause();
+  };
+
+  MediaElementPlayer.prototype.isPlaying = function() {
+    return this._isPlaying;
+  };
+
+  MediaElementPlayer.prototype.isSeeking = function() {
+    return this._mediaElement.seeking;
+  };
+
+  MediaElementPlayer.prototype.getCurrentTime = function() {
+    return this._mediaElement.currentTime;
+  };
+
+  MediaElementPlayer.prototype.getDuration = function() {
+    return this._mediaElement.duration;
+  };
+
+  MediaElementPlayer.prototype.seek = function(time) {
+    this._mediaElement.currentTime = time;
+  };
 
   return MediaElementPlayer;
 });
--- src/player.js
+++ src/player.js
@@ -73,136 +73,136 @@ define([
     self._adapter = adapter;
 
     self._adapter.init(peaks);
+  }
 
-    /**
-     * Cleans up the player object.
-     */
+  /**
+   * Cleans up the player object.
+   */
 
-    Player.prototype.destroy = function() {
-      if (this._interval !== null) {
-        clearTimeout(this._interval);
-        this._interval = null;
+  Player.prototype.destroy = function() {
+    if (this._interval !== null) {
+      clearTimeout(this._interval);
+      this._interval = null;
+    }
+
+    this._adapter.destroy();
+  };
+
+  /**
+   * Starts playback.
+   * @returns {Promise}
+   */
+
+  Player.prototype.play = function() {
+    return this._adapter.play();
+  };
+
+  /**
+   * Pauses playback.
+   */
+
+  Player.prototype.pause = function() {
+    this._adapter.pause();
+  };
+
+  /**
+   * @returns {Boolean} <code>true</code> if playing, <code>false</code>
+   * otherwise.
+   */
+
+  Player.prototype.isPlaying = function() {
+    return this._adapter.isPlaying();
+  };
+
+  /**
+   * @returns {boolean} <code>true</code> if seeking
+   */
+
+  Player.prototype.isSeeking = function() {
+    return this._adapter.isSeeking();
+  };
+
+  /**
+   * Returns the current playback time position, in seconds.
+   *
+   * @returns {Number}
+   */
+
+  Player.prototype.getCurrentTime = function() {
+    return this._adapter.getCurrentTime();
+  };
+
+  /**
+   * Returns the media duration, in seconds.
+   *
+   * @returns {Number}
+   */
+
+  Player.prototype.getDuration = function() {
+    return this._adapter.getDuration();
+  };
+
+  /**
+   * Seeks to a given time position within the media.
+   *
+   * @param {Number} time The time position, in seconds.
+   */
+
+  Player.prototype.seek = function(time) {
+    if (!Utils.isValidTime(time)) {
+      this._peaks.logger('peaks.player.seek(): parameter must be a valid time, in seconds');
+      return;
+    }
+
+    this._adapter.seek(time);
+  };
+
+  /**
+   * Plays the given segment.
+   *
+   * @param {Segment} segment The segment denoting the time region to play.
+   * @param {Boolean} loop If true, playback is looped.
+   */
+
+  Player.prototype.playSegment = function(segment, loop) {
+    var self = this;
+
+    if (!segment ||
+      !Utils.isValidTime(segment.startTime) ||
+      !Utils.isValidTime(segment.endTime)) {
+      self._peaks.logger('peaks.player.playSegment(): parameter must be a segment object');
+      return;
+    }
+
+    clearTimeout(self._interval);
+    self._interval = null;
+
+    // Set audio time to segment start time
+    self.seek(segment.startTime);
+
+    // Start playing audio
+    self.play();
+
+    // We need to use setInterval here as the timeupdate event doesn't fire
+    // often enough.
+    self._interval = setInterval(function() {
+      if (!self.isPlaying()) {
+        clearTimeout(self._interval);
+        self._interval = null;
+        self.pause();
       }
-
-      this._adapter.destroy();
-    };
-
-    /**
-     * Starts playback.
-     * @returns {Promise}
-     */
-
-    Player.prototype.play = function() {
-      return this._adapter.play();
-    };
-
-    /**
-     * Pauses playback.
-     */
-
-    Player.prototype.pause = function() {
-      this._adapter.pause();
-    };
-
-    /**
-     * @returns {Boolean} <code>true</code> if playing, <code>false</code>
-     * otherwise.
-     */
-
-    Player.prototype.isPlaying = function() {
-      return this._adapter.isPlaying();
-    };
-
-    /**
-     * @returns {boolean} <code>true</code> if seeking
-     */
-
-    Player.prototype.isSeeking = function() {
-      return this._adapter.isSeeking();
-    };
-
-    /**
-     * Returns the current playback time position, in seconds.
-     *
-     * @returns {Number}
-     */
-
-    Player.prototype.getCurrentTime = function() {
-      return this._adapter.getCurrentTime();
-    };
-
-    /**
-     * Returns the media duration, in seconds.
-     *
-     * @returns {Number}
-     */
-
-    Player.prototype.getDuration = function() {
-      return this._adapter.getDuration();
-    };
-
-    /**
-     * Seeks to a given time position within the media.
-     *
-     * @param {Number} time The time position, in seconds.
-     */
-
-    Player.prototype.seek = function(time) {
-      if (!Utils.isValidTime(time)) {
-        this._peaks.logger('peaks.player.seek(): parameter must be a valid time, in seconds');
-        return;
-      }
-
-      this._adapter.seek(time);
-    };
-
-    /**
-     * Plays the given segment.
-     *
-     * @param {Segment} segment The segment denoting the time region to play.
-     * @param {Boolean} loop If true, playback is looped.
-     */
-
-    Player.prototype.playSegment = function(segment, loop) {
-      var self = this;
-
-      if (!segment ||
-        !Utils.isValidTime(segment.startTime) ||
-        !Utils.isValidTime(segment.endTime)) {
-        self._peaks.logger('peaks.player.playSegment(): parameter must be a segment object');
-        return;
-      }
-
-      clearTimeout(self._interval);
-      self._interval = null;
-
-      // Set audio time to segment start time
-      self.seek(segment.startTime);
-
-      // Start playing audio
-      self.play();
-
-      // We need to use setInterval here as the timeupdate event doesn't fire
-      // often enough.
-      self._interval = setInterval(function() {
-        if (!self.isPlaying()) {
+      else if (self.getCurrentTime() >= segment.endTime) {
+        if (loop) {
+          self.seek(segment.startTime);
+        }
+        else {
           clearTimeout(self._interval);
           self._interval = null;
           self.pause();
         }
-        else if (self.getCurrentTime() >= segment.endTime) {
-          if (loop) {
-            self.seek(segment.startTime);
-          }
-          else {
-            clearTimeout(self._interval);
-            self._interval = null;
-            self.pause();
-          }
-        }
-      }, 30);
-    };
-  }
+      }
+    }, 30);
+  };
 
   return Player;
 });
