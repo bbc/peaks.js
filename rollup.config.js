464b1ed487ff839b321a30ddb3348d0e63625883
Chris Needham <chris.needham@bbc.co.uk>
2021-06-20 20:57:44 +0100

    Updated rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import { babel } from '@rollup/plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
