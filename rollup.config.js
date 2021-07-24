fb88d10f6d4d24d1efb541adc4ceedb972cbf794
Adi Sahar <adi@deepdub.ai>
2021-07-24 19:32:19 +0300

    fix demos

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import externalGlobals from 'rollup-plugin-external-globals';
 
 import path from 'path';
 
@@ -45,12 +46,11 @@ export default {
   ],
   // eslint-disable-next-line
   // https://github.com/formium/tsdx/blob/462af2d002987f985695b98400e0344b8f2754b7/src/createRollupConfig.ts#L63
-  external: (id) => {
+  external: process.env.INCLUDE_DEPS ? [] : (id) => {
     // bundle in polyfills as TSDX can't (yet) ensure they're installed as deps
     if (id.startsWith('regenerator-runtime')) {
       return false;
     }
-
     return !id.startsWith('.') && !path.isAbsolute(id);
   },
   watch: {
@@ -59,6 +59,17 @@ export default {
   plugins: [
     commonjs(),
     resolve({ browser: true }),
-    babel({ babelHelpers: 'bundled' })
+    babel({ babelHelpers: 'bundled' }),
+    // https://github.com/rollup/rollup/issues/2374
+    // https://github.com/rollup/rollup-plugin-babel/issues/162
+    process.env.INCLUDE_DEPS ? null : externalGlobals({
+      'konva/lib/Core': 'Konva',
+      'konva/lib/Animation': 'Konva',
+      'konva/lib/shapes/Line': 'Konva',
+      'konva/lib/shapes/Rect': 'Konva',
+      'konva/lib/shapes/Text': 'Konva',
+      'waveform-data': 'WaveformData',
+      'eventemitter3': 'EventEmitter3'
+    })
   ]
 };
