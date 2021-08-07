0207a452dbba5df8e9edb7c9573e3a0363b181e4
Chris Needham <chris.needham@bbc.co.uk>
2021-08-07 15:09:14 +0100

    Added ES module build
    - Moved build outputs to dist folder
    - Moved waveform-data to peerDependencies
    - Use rollup-plugin-peer-deps-external library to automatically
      mark peer dependencies as external in the ES module build

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,62 @@
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: 'src/main.js',
-  output: [
-    {
-      file: 'peaks.js',
-      name: 'peaks',
-      format: 'umd',
-      sourcemap: true,
-      freeze: false
-    },
-    {
-      file: 'peaks.min.js',
-      name: 'peaks',
-      format: 'umd',
-      sourcemap: true,
-      freeze: false,
-      plugins: [
-        terser()
-      ]
-    }
-  ],
-  external: [],
-  watch: {
-    include: 'src/**',
+export default [
+  {
+    input: 'src/main.js',
+    output: [
+      {
+        file: 'dist/peaks.js',
+        name: 'peaks',
+        format: 'umd',
+        sourcemap: true,
+        freeze: false
+      },
+      {
+        file: 'dist/peaks.min.js',
+        name: 'peaks',
+        format: 'umd',
+        sourcemap: true,
+        plugins: [
+          terser()
+        ]
+      }
+    ],
+    external: [],
+    plugins: [
+      commonjs(),
+      resolve({ browser: true }),
+      babel({ babelHelpers: 'bundled' })
+    ]
   },
-  plugins: [
-    commonjs(),
-    resolve({ browser: true }),
-    babel({ babelHelpers: 'bundled' })
-  ]
-};
+  {
+    input: 'src/main.js',
+    output: [
+      {
+        file: 'dist/peaks.esm.js',
+        name: 'peaks',
+        format: 'es',
+        sourcemap: true,
+        freeze: false,
+      },
+      {
+        file: 'dist/peaks.esm.min.js',
+        name: 'peaks',
+        format: 'es',
+        sourcemap: true,
+        plugins: [
+          terser()
+        ]
+      }
+    ],
+    plugins: [
+      peerDepsExternal(),
+      commonjs(),
+      resolve({ browser: true }),
+      babel({ babelHelpers: 'bundled' })
+    ]
+  }
+];
