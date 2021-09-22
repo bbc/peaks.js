564db81afb503e0393637631175b1958f062bd9c
Chris Needham <chris.needham@bbc.co.uk>
2021-09-22 18:09:14 +0100

    Added peaks.ext.js and peaks.ext.min.js builds
    These are UMD builds with Konva.js and waveform-data.js
    as external libraries (hence "ext") and referenced via
    global variables (Konva and WaveformData)

--- rollup.config.js
+++ rollup.config.js
@@ -7,11 +7,22 @@ import path from 'path';
 
 import pkg from './package.json';
 
+const globals = {
+  'waveform-data': 'WaveformData',
+  'konva/lib/Core': 'Konva',
+  'konva/lib/Animation': 'Konva',
+  'konva/lib/shapes/Line': 'Konva',
+  'konva/lib/shapes/Rect': 'Konva',
+  'konva/lib/shapes/Text': 'Konva'
+};
+
 function sourcemapPathTransform(sourcePath) {
   return path.join('node_modules', pkg.name, './src', sourcePath);
 }
 
 export default [
+  // dist/peaks.js and dist/peaks.min.js are stand-alone UMD modules that
+  // contain all dependencies. Custom markers do not work with these builds
   {
     input: 'src/main.js',
     output: [
@@ -41,6 +52,8 @@ export default [
       babel({ babelHelpers: 'bundled' })
     ]
   },
+  // dist/peaks.esm.js is an ES module bundle that does not include Konva.js
+  // and waveform-data.js dependencies
   {
     input: 'src/main.js',
     output: [
@@ -51,6 +64,37 @@ export default [
         sourcemap: true,
         sourcemapPathTransform,
         freeze: false
+      }
+    ],
+    plugins: [
+      peerDepsExternal(),
+      commonjs(),
+      resolve({ browser: true }),
+      babel({ babelHelpers: 'bundled' })
+    ]
+  },
+  // dist/peaks.ext.js and dist/peaks.ext.min.js are UMD builds that do not
+  // include Konva.js and waveform-data.js dependencies
+  {
+    input: 'src/main.js',
+    output: [
+      {
+        file: 'dist/peaks.ext.js',
+        name: 'peaks',
+        format: 'umd',
+        sourcemap: true,
+        freeze: false,
+        globals
+      },
+      {
+        file: 'dist/peaks.ext.min.js',
+        name: 'peaks',
+        format: 'umd',
+        sourcemap: true,
+        globals,
+        plugins: [
+          terser()
+        ]
       }
     ],
     plugins: [
