246f793a4157424bc7caa2b6fa5ae0c25ef09eea
Chris Needham <chris@chrisneedham.com>
2023-03-02 22:16:36 +0000

    Reduce overlay marker handle width
    See #479

--- src/overlay-segment-marker.js
+++ src/overlay-segment-marker.js
@@ -25,7 +25,7 @@ function OverlaySegmentMarker(options) {
 }
 
 OverlaySegmentMarker.prototype.init = function(group) {
-  const handleWidth  = 30;
+  const handleWidth  = 10;
   const handleHeight = 20;
   const handleX      = -(handleWidth / 2) + 0.5; // Place in the middle of the marker
 
